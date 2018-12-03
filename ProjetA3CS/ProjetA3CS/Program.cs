09970af79eac27bc1a96aa283f75b75a51845c97
gregory.rebus <gregory.rebus@viacesi.fr>
2018-12-03 11:17:52 +0100

    test

--- ProjetA3CS/ProjetA3CS/Program.cs
+++ ProjetA3CS/ProjetA3CS/Program.cs
@@ -17,6 +17,7 @@ namespace ProjetA3CS
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+            //
         }
     }
 }
